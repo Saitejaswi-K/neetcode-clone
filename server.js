dcd5a0852bbb4bfd18539cd539c6dae7aaa25d1e
Saitejaswi_k <20wh1a1206@bvrithyderabad.edu.in>
2023-02-26 14:16:44 +0530

    Mongoose installation

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const str = process.env.MONGO_CONNECTION_STRING;
 const express = require("express");
+const mongoDB =  require("mongoose");
+
+
 
 const app = express();
 app.use(express.static("frontend"));
@@ -8,7 +14,19 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/frontend/index.html");
 })
 
-app.listen(3000, function(){
+/*app.listen(3000, function(){
     console.log("Server is running on http://localhost:3000");
-})
+})*/
 
+mongoDB.set('strictQuery', false)
+mongoDB.connect(process.env.MONGO_CONNECTION_STRING, function(err){
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log("Successfull DB connection");
+        app.listen(3000, function(){
+            console.log("Server is running on http://localhost:3000");
+        })   
+    }
+});
